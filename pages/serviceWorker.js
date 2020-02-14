0ae29ce45676333af3eab6179eee88346eb04913
Mudi Ogra <ograabiodun@gmail.com>
2020-02-14 06:55:50 +0100

    testing git hub pages2

--- pages/serviceWorker.js
+++ pages/serviceWorker.js
@@ -1,7 +1,7 @@
 const cacheName = 'v1';
 const cacheFiles = [
     '../',
-    './index.html',
+    '../index.html',
     '../pages/userBoard.html',
     '../pages/adminBoard.html',    
     '../pages/register.html',
